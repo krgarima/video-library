4b12a35909b7b9dfe2bafdd3dee23d991dceee47
Garima <kumargarima59@gmail.com>
2022-05-18 17:05:52 +0530

    feat: add to and remove from watch later

--- src/app/store.js
+++ src/app/store.js
@@ -1,8 +1,10 @@
 import { configureStore } from "@reduxjs/toolkit";
 import likesReducer from "../features/like/like-slice";
+import watchLaterReducer from "../features/watchLater/watchLater-slice";
 
 export const store = configureStore({
   reducer: {
     likes: likesReducer,
+    watchLater: watchLaterReducer,
   },
 });
---
+++ src/features/watchLater/watchLater-slice.js
@@ -0,0 +1,77 @@
+import { createSlice, createAsyncThunk } from "@reduxjs/toolkit";
+import axios from "axios";
+
+let encodedToken = "";
+
+const initialState = {
+  watchLater: [],
+  status: null,
+};
+
+export const addToWatchLater = createAsyncThunk(
+  "watchLater/addToWatchLater",
+  async (video) => {
+    try {
+      encodedToken = localStorage.getItem("token");
+      const res = await axios.post(
+        "/api/user/watchlater",
+        { video },
+        {
+          headers: {
+            authorization: encodedToken,
+          },
+        }
+      );
+      return res.data.watchlater;
+    } catch (error) {
+      console.log(error);
+    }
+  }
+);
+
+export const removeFromWatchLater = createAsyncThunk(
+  "watchLater/removeFromWatchLater",
+  async (videoId) => {
+    try {
+      encodedToken = localStorage.getItem("token");
+      const res = await axios.delete(`/api/user/watchlater/${videoId}`, {
+        headers: {
+          authorization: encodedToken,
+        },
+      });
+      return res.data.watchlater;
+    } catch (error) {
+      console.log(error);
+    }
+  }
+);
+
+const watchLaterSlice = createSlice({
+  name: "watchLater",
+  initialState,
+  extraReducers: {
+    [addToWatchLater.pending]: (state) => {
+      state.status = "loading";
+    },
+    [addToWatchLater.fulfilled]: (state, action) => {
+      state.watchLater = action.payload;
+      state.status = "success";
+    },
+    [addToWatchLater.rejected]: (state) => {
+      state.status = "failed";
+    },
+
+    [removeFromWatchLater.pending]: (state) => {
+      state.status = "loading";
+    },
+    [removeFromWatchLater.fulfilled]: (state, action) => {
+      state.watchLater = action.payload;
+      state.status = "success";
+    },
+    [removeFromWatchLater.rejected]: (state) => {
+      state.status = "failed";
+    },
+  },
+});
+
+export default watchLaterSlice.reducer;
