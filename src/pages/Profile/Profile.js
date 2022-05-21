7a770d57200eedcc10a80325f89915bee9b6f832
Garima <kumargarima59@gmail.com>
2022-05-21 14:37:55 +0530

    feat: user can update profile

--- src/pages/Profile/Profile.js
+++ src/pages/Profile/Profile.js
@@ -1,8 +1,23 @@
-import React from "react";
+import React, { useState } from "react";
 import Filters from "../../components/Filters/Filters";
 import "./Profile.css";
 
+let user = JSON.parse(localStorage.getItem("user")) ?? {
+  name: "Adarsh Balika",
+  email: "adarshbalika@gmail.com",
+  mobile: "+91 9876543210",
+  address: "0, Unknown address",
+  dob: "10/10/9999",
+};
+
 export default function Profile() {
+  const [userData, setUserData] = useState(user);
+
+  const handleSubmit = (e) => {
+    e.preventDefault();
+    localStorage.setItem("user", JSON.stringify(userData));
+  };
+
   return (
     <aside className="home">
       <Filters />
@@ -10,21 +25,50 @@ export default function Profile() {
         <div className="user-profile">
           <h1>Profile</h1>
           <label htmlFor="name">Full Name: </label>
-          <input type="text" name="" id="name" value="Adarsh Balika" readOnly />
+          <input
+            type="text"
+            name=""
+            id="name"
+            value={userData.name}
+            onChange={(e) => setUserData({ ...userData, name: e.target.value })}
+          />
           <label htmlFor="email">Email: </label>
           <input
             type="text"
             id="email"
-            value="adarshbalika@gmail.com"
-            readOnly
+            value={userData.email}
+            onChange={(e) =>
+              setUserData({ ...userData, email: e.target.value })
+            }
           />
           <label htmlFor="phone">Mobile: </label>
-          <input type="text" id="phone" value="+91 9876543210" readOnly />
+          <input
+            type="text"
+            id="phone"
+            value={userData.mobile}
+            onChange={(e) =>
+              setUserData({ ...userData, mobile: e.target.value })
+            }
+          />
           <label htmlFor="address">Address: </label>
-          <input type="text" id="address" value="0, Unknown address" readOnly />
+          <input
+            type="text"
+            id="address"
+            value={userData.address}
+            onChange={(e) =>
+              setUserData({ ...userData, address: e.target.value })
+            }
+          />
           <label htmlFor="dob">DOB: </label>
-          <input type="text" id="dob" value="10/10/9999" readOnly />
-          <button>Save</button>
+          <input
+            type="text"
+            id="dob"
+            value={userData.dob}
+            onChange={(e) => setUserData({ ...userData, dob: e.target.value })}
+          />
+          <button type="submit" onClick={handleSubmit}>
+            Save
+          </button>
         </div>
       </div>
     </aside>
