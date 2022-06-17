308e5b94c46e94d410ffd152e4d3addc8f1e2b65
Garima <kumargarima59@gmail.com>
2022-06-17 15:16:48 +0530

    fix: error

--- src/pages/Login/Login.js
+++ src/pages/Login/Login.js
@@ -1,4 +1,4 @@
-import React, { useState, useContext } from "react";
+import React, { useState, useContext, useEffect } from "react";
 import { Link } from "react-router-dom";
 import { AuthContext } from "../../context/auth-context";
 import axios from "axios";
